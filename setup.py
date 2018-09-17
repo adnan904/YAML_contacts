7f8dcda329f78438ca5dfd60bbf058e6cadbe2b9
adnan904 <34026698+adnan904@users.noreply.github.com>
2018-09-17 14:07:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,9 @@ setup(
     version='1.0',
     url='https://github.com/adnan904/YAML_contacts',
     author='Adnan Manzoor',
-    description='A Python3 Program from storing contacts in a YAML structured File.',
+    description='A Python3 Program for storing contacts in a YAML structured File.',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'pycontacts = contacts.contacts:main',
-        ]
-    },
+    entry_points={'console_scripts': ['pycontacts = contacts.contacts:main']},
     install_requires=[
           'PyYAML' , 'argparse'
       ]
